3d64b0b5a6fbd502b72d0b3b0702a3047868c4f2
Vinal Munasinghe <vinalmunasinghe@gmail.com>
2024-03-22 11:59:40 +0530

    Creation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,17 @@ public class 	DemoApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<student> hello() {
+		return List.of(
+				new student(
+						1L,
+						"Mariam",
+						"Mariam.jamal@gmail.com",
+						LocalDate.of(2000, Month.JANUARY,	23),
+						21
+
+				)
+		);
 	}
 
 }
